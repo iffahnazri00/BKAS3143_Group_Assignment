e7d0990406e87bd933b1243e2208b9d8704a0d41
iffahnazri00 <93260077+iffahnazri00@users.noreply.github.com>
2022-02-19 16:41:40 +0800

    Add files via upload

---
+++ customers/includes/db.php
@@ -0,0 +1,9 @@
+<?php
+	$servername = "localhost"; //Server for xampp is localhost
+	$username = "root"; //Default xampp is root
+	$password = ""; //Use the pass
+	$dbname = "sstho";
+
+	//To create the connection
+	$con = mysqli_connect("localhost","root","","sstho");
+?>
